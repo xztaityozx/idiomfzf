cc93d9815e1fc1ebf08a596efb7343b05995ff8c
xztaityozx <taityonohibi@gmail.com>
2017-10-18 23:22:09 +0900

    first commit

---
+++ idiomfzf.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+touch ~/.idioms
+
+if [ "$1" == "add" ]; then
+  
+  name=""
+  ans="n"
+  while [ "$name" == "" ] || [ "$ans" != "y" ]; do
+    echo -en "\e[1;39mIdiom Name : \e[0;39m"
+    read name
+    echo -en "\e[1mIs name Ok?(y/n) \e[0m: "
+    read ans
+  done
+  
+  
+  name=$(echo $name|sed 's/ /_/g')
+  
+  shift
+  com="$@"
+
+  if [ "$com" == "[:last:]" ] || [ "$com" == "" ]; then
+    com=$(cat ~/.bash_history|tail -n2|head -n1)
+  fi
+  
+  echo "[$name] "$com"" >> ~/.idioms
+
+  echo "Complete Add idioms"
+  exit
+fi
+
+if [ "$1" == "" ]; then
+  com=$(cat ~/.idioms|while read LINE;do
+      echo $LINE|sed 's/\(\[.*\]\)/\\\\e[1;33m\1\\\\e[0;39m/g'|xargs echo -e
+    done|fzf|awk '{$1="";print $0}'|sed 's/^ //g'
+  )
+  if [ "$idiom_DO" == "1" ];then
+    eval $com
+  else
+    echo $com
+  fi
+fi
